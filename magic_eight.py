fbee648d066bb60094db41566edc0fb4c336c6b9
Geoff Pottow <gpottow@umich.edu>
2018-01-31 17:04:00 -0500

    added question checker

--- magic_eight.py
+++ magic_eight.py
@@ -1,3 +1,10 @@
 
 
-print("What's your question?")
+
+run = True
+while run:
+    user_input = input("What's your question?")
+    if user_input.upper() == "QUIT":
+        run = False
+    elif not user_input.endswith('?'):
+        print("I’m sorry, I can only answer questions.")
